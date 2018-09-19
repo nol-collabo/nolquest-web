c3a9c6fce9d84bf91891b15c04ffcb65d34218a6
Saemii <wrfg12@gmail.com>
2018-09-19 18:19:26 +0900

    [update] mission_issue_check_loading route

--- targets.js
+++ targets.js
@@ -3,7 +3,8 @@ var listItem = {
     'index.html',
     'list.html',
     'mission_issue01.html',
-    'mission_issue_no.html'
+    'mission_issue_no.html',
+    'mission_issue_check_loading.html'
   ]
 }
 
