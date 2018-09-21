a83a96e74d07d3b2db721979c49ed7ec4e5b70ed
aki_pro <aki@edkim.co>
2018-09-21 18:57:23 +0900

    node export

--- targets.js
+++ targets.js
@@ -1,23 +1,24 @@
 var listItem = {
   list: [
-    'index.html',
-    'list.html',
-    'info_environment.html',
-    'start.html',
-    'login.html',
-    'input_name.html',
-    'guide.html',
-    'interest.html',
-    'my_status.html',
-    'mission_issue01.html',
-    'mission_issue_no.html',
-    'mission_issue_check_loading.html',
-    'mission_issue_check.html',
-    'mission_correct.html',
-    'mission_levelup.html',
-    'mission_wrong.html',
-    'mission_wrong02.html'
+    "index_mission_empty.html",
+    "index.html",
+    "list.html",
+    "info_environment.html",
+    "start.html",
+    "login.html",
+    "input_name.html",
+    "guide.html",
+    "interest.html",
+    "my_status.html",
+    "mission_issue01.html",
+    "mission_issue_no.html",
+    "mission_issue_check_loading.html",
+    "mission_issue_check.html",
+    "mission_correct.html",
+    "mission_levelup.html",
+    "mission_wrong.html",
+    "mission_wrong02.html"
   ]
-}
+};
 
-module.exports = listItem
+module.exports = listItem;
