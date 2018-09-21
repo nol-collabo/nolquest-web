011d55e71ec45197573a66fc2edd3528fde63114
aki_pro <aki@edkim.co>
2018-09-21 17:02:34 +0900

    [update] 아이템 코드 입력시 팝업 노출.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {});
+$(document).ready(function() {});
 
 // 이 미션 포기하기
 function showGiveupModal() {
@@ -7,9 +7,9 @@ function showGiveupModal() {
 
 // 아이템 코드번호 입력
 function setCode(id) {
-  // 테스트를 위해 임의로 코드 111로 매칭 
+  // 테스트를 위해 임의로 코드 111로 매칭
   if ($("[name=code" + id + "]").val() == 111) {
-
+    $("#itemModal").modal();
   } else {
     $("#errorCodeModal").modal();
   }
@@ -21,24 +21,18 @@ function showHintModal() {
 }
 
 function getHint() {
-  $("#hintConfirmModal").modal('hide');
+  $("#hintConfirmModal").modal("hide");
 
-  $(".hint__card").not(".hint__card--details").addClass("hidden");
+  $(".hint__card")
+    .not(".hint__card--details")
+    .addClass("hidden");
   $(".hint__card--details").removeClass("hidden");
 }
 
-// 잔여 시간 타이머 
+// 잔여 시간 타이머
 function showTimerModal() {
   $("#timerModal").modal();
 }
-
-
-
-
-
-
-
-
 
 /**
  * cookie
@@ -47,22 +41,27 @@ function setCookie(name, value, expiredays) {
   var today = new Date();
   today.setDate(today.getDate() + expiredays);
 
-  document.cookie = name + '=' + escape(value) + '; path=/; expires=' + today.toGMTString() + ';'
+  document.cookie =
+    name +
+    "=" +
+    escape(value) +
+    "; path=/; expires=" +
+    today.toGMTString() +
+    ";";
 }
 
 function getCookie(name) {
   var cName = name + "=";
   var x = 0;
   while (x <= document.cookie.length) {
-    var y = (x + cName.length);
+    var y = x + cName.length;
     if (document.cookie.substring(x, y) == cName) {
       if ((endOfCookie = document.cookie.indexOf(";", y)) == -1)
         endOfCookie = document.cookie.length;
       return unescape(document.cookie.substring(y, endOfCookie));
     }
     x = document.cookie.indexOf(" ", x) + 1;
-    if (x == 0)
-      break;
+    if (x == 0) break;
   }
   return "";
 }
