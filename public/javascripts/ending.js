76daf0cdcb50af1f5cba49617b0134fd99621893
amiru11 <wrfg12@gmail.com>
2018-09-27 14:56:36 +0900

    [update] ending mission list 최대 3개까지 체크가능하도록 변경

--- public/javascripts/ending.js
+++ public/javascripts/ending.js
@@ -1,7 +1,24 @@
 $(function () {
   let morePopup = $('.more');
-  
+  let checkbox = $('.mission-check__box--header > input');
+
   morePopup.click(function() {
     $('#itemModal').modal('show');
   });
+
+  checkbox.change(function() {
+    let checked = $('.mission-check__box--header > input:checked');
+    let checkedLength = checked.length;
+    if(checkedLength > 0 && checkedLength < 4) {
+      $('.ending__button button').addClass('btn-blue');
+      $('.ending__button button').prop('disabled', false);
+    } else if(checkedLength > 3) {
+      alert('더이상 선택할 수 없습니다.');
+      $(this).prop('checked', false);
+    } else {
+      $('.ending__button button').removeClass('btn-blue');
+      $('.ending__button button').prop('disabled', true);
+    }
+  })
+
 })
