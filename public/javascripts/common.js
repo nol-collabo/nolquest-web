12034a7342ad2bb3379e0f162f929342bce85bb9
aki_pro <aki@edkim.co>
2018-11-14 17:11:32 +0900

    scroll 테스트.

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -2,6 +2,18 @@ function goTo(path) {
   location.href = path;
 }
 
+$(document).on("shown.bs.modal", ".modal", function() {
+  var top =
+    window.pageYOffset ||
+    document.documentElement.scrollTop ||
+    document.body.scrollTop ||
+    0;
+
+  $("body").css("top", -top);
+  $("body").addClass("modal-noscroll");
+});
+
 $(document).on("hidden.bs.modal", ".modal", function() {
-  $("body").toggleClass("modal-open", $(".modal").hasClass("in"));
+  $("body").css("top", 0);
+  $("body").removeClass("modal-noscroll");
 });
